9da1c8c306b67af9e22349c272563b243b3de860
Riley Miller <rmllr000@gmail.com>
2020-02-12 00:41:40 -0700

    added LotsView

---
+++ src/components/LotsView/LotsView.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { useNavigation } from 'hooks/useNavigation'
+
+
+const LotsView = () => {
+  return (
+    <Text>
+      yo
+    </Text>
+  )
+}
+
+export default LotsView
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-
+import LotsView from 'components/LotsView/LotsView'
 import { useNavigation } from 'hooks/useNavigation'
 import { colors } from 'colors/colors'
 const HomeScreen = () => {
   const navigation = useNavigation();
   return (
     <View style={{ flex: 1, backgroundColor: colors.backgroundPrimaryColor }}>
-      <Text testID="title">Home Page</Text>
+      <LotsView />
     </View>
   )
 }
